824c0cceedf8bdca92a6690d0b3b79acfa9af4e1
Julyfun MacOS14.5 M1 <43675484+julyfun@users.noreply.github.com>
2024-09-02 15:40:37 +0800

    M .tmux.conf | M one-command-config.bash

--- one-command-config.bash
+++ one-command-config.bash
@@ -1,22 +1,38 @@
 # - 设置镜像源
 # https://mirrors.tuna.tsinghua.edu.cn/help/ubuntu/
-# - setup github ssh keys manually
-# ssh-keygen -t rsa
 # https://github.com/settings/ssh/new
-# - sudo apt install git
-# - sudo apt install curl
 # - curl -o one-command-config.bash https://raw.githubusercontent.com/julyfun/personal-cfg/main/one-command-config.bash && yes | bash one-command-config.bash
 
+if [ "$(id -u)" -ne 0]; then
+    alias sudo='echo Non-root user, sudo is ignored:'
+fi
+
+sc22=\
+'deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
+deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
+'
+ver=$(grep VERSION_ID /etc/os-release | cut -d '"' -f 2 | tr -d '"')
+if [ "$ver" = "22.04"  ]; then
+    sudo echo "$sc22" > /etc/apt/sources.list
+fi
+
+# 以下安全更新软件源包含了官方源与镜像站配置，如有需要可自行修改注释切换
+sudo apt update
+sudo apt install git
+sudo apt install vim # you can do anything with this editor
+sudo apt install curl
 # Sometimes failed
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
 sudo apt install fish
 mkdir -p ~/Documents/GitHub
 cd ~/Documents/GitHub
-git clone git@github.com:julyfun/jst.fish --depth=10
-git clone git@github.com:julyfun/kickstart.fish --depth=10
-git clone git@github.com:julyfun/personal-cfg --depth=10
-git clone git@github.com:wting/autojump.git --depth=10
+git clone https://github.com/julyfun/jst.fish --depth=10
+git clone https://github.com/julyfun/kickstart.fish --depth=10
+git clone https://github.com/julyfun/personal-cfg --depth=10
+git clone https://github.com/autojump.git --depth=10
 cd autojump
 ./install.py
 
@@ -36,10 +52,12 @@ echo "$fish_source" >> ~/.config/fish/config.fish
 
 # # Sometimes failed 可能只有配好了 clash 才行
 mkdir -p ~/.mfa/bin
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+curl -LO https://mirror.ghproxy.com/https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 mv nvim.appimage ~/.mfa/bin/nvim
 echo 'set PATH "$PATH:$HOME/.mfa/bin"' >> ~/.config/fish/config.fish
+
+exec fish
 
 # sudo add-apt-repository ppa:neovim-ppa/stable
 # sudo apt-get update
