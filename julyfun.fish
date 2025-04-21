9c2f544c1b7d33b1c88738bae78842bfe99aae0b
julyfun-4070s-ubuntu2204 <julyfun.collect@outlook.com>
2025-04-22 02:36:34 +0800

    ?? julyfun.fish | ?? vscode/extensions.md

---
+++ julyfun.fish
@@ -0,0 +1,3 @@
+alias note="nvim /home/julyfun/Documents/GitHub/julyfun/how-to/notes/julyfun/240730-摸鱼日志.md"
+alias notej="cd /home/julyfun/Documents/GitHub/julyfun/how-to/notes/julyfun/"
+
---
+++ vscode/extensions.md
@@ -0,0 +1,8 @@
+## 25.4.22
+
+- vim
+- python
+- copilot
+- error lens
+- [?] gitlens
+
