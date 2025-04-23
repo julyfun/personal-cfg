bac446caffbc3729437ab8fceb64ae066451737b
julyfun-4070s-ubuntu2204 <julyfun.collect@outlook.com>
2025-04-24 04:18:49 +0800

    M auto/top.sh

--- auto/top.sh
+++ auto/top.sh
@@ -1,6 +1,6 @@
 tmux new-session -d -s top
 tmux send-keys -t top:0 "btop" C-m
-tmux split-window -h -t top:0
+tmux split-window -v -t top:0
 tmux send-keys -t top:0.1 "nvtop" C-m
 tmux attach -t "top"
 
