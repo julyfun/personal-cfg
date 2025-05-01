da227252dac013897648799965aefb4701b3aa82
Guanhuai04 <guanhuai1006@126.com>
2025-05-01 16:42:05 +0800

    M auto/auto.sh

--- auto/auto.sh
+++ auto/auto.sh
@@ -4,5 +4,5 @@ tmux new-session -d -s auto
 tmux send-keys -t auto:0 "export DISPLAY=:0" C-m
 tmux send-keys -t auto:0 "rustdesk" C-m
 tmux split-window -v -t auto:0
-tmux send-keys -t auto:0.1 "cd; j how-to" C-m
-tmux send-keys -t auto:0.1 "watch -n 600 fish -c 'jp'" C-m
+tmux send-keys -t auto:0.1 "watch -n 600 \"fish -c 'cd; j how-to; jp'\"" C-m
+
