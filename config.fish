74910e5d8c722546585d601a2cae69eed30b82e0
Julyfun M4 <julyfun.collect@outlook.com>
2025-08-13 17:16:55 +0800

    M config.fish

--- config.fish
+++ config.fish
@@ -3,3 +3,12 @@ alias note="nvim $HOW/notes/julyfun/240730-摸鱼日志.md"
 alias notej="cd $HOW/notes/julyfun/"
 alias how="jd ~/Documents/GitHub/julyfun/how-to $argv"
 
+set -gx HF_ENDPOINT "https://hf-mirror.com"
+
+export RUSTUP_DIST_SERVER="https://rsproxy.cn"
+export RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"
+
+export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+export HOMEBREW_INSTALL_FROM_API=1
+
