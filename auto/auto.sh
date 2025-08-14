4648ae27b43f9f8aaafd37e7266348d8b3861ea9
Julyfun M4 <julyfun.collect@outlook.com>
2025-08-14 19:21:04 +0800

    M auto/auto.sh

--- auto/auto.sh
+++ auto/auto.sh
@@ -4,5 +4,7 @@ tmux new-session -d -s auto \
 && tmux send-keys -t auto:0 "export DISPLAY=:0" C-m \
 && tmux send-keys -t auto:0 "rustdesk" C-m \
 && tmux split-window -v -t auto:0 \
-&& tmux send-keys -t auto:0.1 "watch -n 600 \"fish -c 'cd ~/Documents/GitHub/julyfun/how-to; jp'\"" C-m
+&& tmux send-keys -t auto:0.1 "watch -n 600 \"fish -c 'cd ~/Documents/GitHub/julyfun/how-to; jp'\"" C-m \
+&& tmux split-window -h -t auto:0 \
+&& tmux send-keys -t auto:0.2 "watch -n 600 \"fish -c 'cd ~/Documents/GitHub/julyfun/buffer; jp'\"" C-m
 
