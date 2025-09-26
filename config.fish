22bde11557bab1a3f686f1776a7af32124847087
Julyfun M4 <julyfun.collect@outlook.com>
2025-09-26 16:06:40 +0800

    M config.fish

--- config.fish
+++ config.fish
@@ -3,6 +3,8 @@ alias note="nvim $HOW/notes/julyfun/240730-摸鱼日志.md"
 alias notej="cd $HOW/notes/julyfun/"
 alias how="jd ~/Documents/GitHub/julyfun/how-to $argv"
 alias key="nvim ~/.config/nvim/lua/plugins/snacks.lua"
+alias do="nvim ~/Documents/GitHub/julyfun/buffer/doing.md"
+alias buf="nvim ~/Documents/GitHub/julyfun/buffer/buffer.md"
 
 set -gx HF_ENDPOINT "https://hf-mirror.com"
 
