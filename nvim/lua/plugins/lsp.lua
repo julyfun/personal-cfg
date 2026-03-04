3e4335575ddd1c4f2c71011c607abb4c95e2d6a3
Julyfun MacOS14.5 M1 <43675484+julyfun@users.noreply.github.com>
2026-03-04 20:15:23 +0800

    M config.fish | M nvim/lua/plugins/lsp.lua | M vscode/vim.settings.json | ?? ai/rss.md

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,6 +5,17 @@ return {
         config = function()
             -- For Neovim 0.11+, use vim.lsp.config
             vim.lsp.config('sourcekit', {})
+            vim.lsp.config('zls', {
+                cmd = { vim.fn.expand('$HOME/app/zls') },
+                filetypes = { 'zig', 'zon' },
+                root_markers = { 'build.zig', 'build.zig.zon' },
+                settings = {
+                    zls = {
+                        zig_exe_path = vim.fn.expand('$HOME/app/zig'),
+                    }
+                },
+            vim.lsp.enable('zls')
+            })
         end,
     }
 }
